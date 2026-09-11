2d1811b1e8fd19ed9cc53bea346e5eef6851daae
moahmed-fahad <crfahad132@gmail.com>
2026-09-11 21:05:01 +0530

    Mount authentication routes

--- server.cjs
+++ server.cjs
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const authRoutes = require("./routes/authRoutes");
 
 const db = require("./config/db");
 const verifyToken = require("./middleware/authMiddleware");
@@ -18,6 +19,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/", authRoutes);
 
 app.use("/", dashboardRoutes);
 app.use("/", productRoutes);
